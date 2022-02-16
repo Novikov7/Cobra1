2744f24b2d8d0216ee2f7e5c8da0cd344051e9aa
Oleg Novikov <novikov7@yahoo.com>
2022-02-16 10:58:00 +0300

    first commit

--- app/src/main/java/com/example/cobra1/MainActivity.java
+++ app/src/main/java/com/example/cobra1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.cobra1;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: ");
     }
 }
