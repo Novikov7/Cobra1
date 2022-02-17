9c6404bcc1b6880b9993153cc659bf8a6da23f62
Oleg Novikov <novikov7@yahoo.com>
2022-02-17 16:50:09 +0300

    1

--- app/src/main/java/com/example/cobra1/MainActivity.java
+++ app/src/main/java/com/example/cobra1/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setContentView(R.layout.activity_main); setContentView(R.layout.activity_main); setContentView(R.layout.activity_main);
-
         Log.i(TAG, "onCreate: ");
-
+        //1
+    }
 }
